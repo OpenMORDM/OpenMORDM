f912f69abc4be3766849442ee58fccf9d158fb56
David Hadka <dmh309@arl.psu.edu>
2014-04-24 16:42:57 -0400

    Work on plotting and other analyses

--- OpenMORDM.R
+++ OpenMORDM.R
@@ -1,6 +1,10 @@
 library(grid)
 library(prim)
 library(plot3D)
+library(MASS)
+library(hypervolume)
+library(rgl)
+library(animation)
 
 mordm.defaultnames <- function(nvars, nobjs) {
 	names <- vector()
@@ -85,6 +89,20 @@ mordm.attributes <- function(data) {
 	return(result)
 }
 
+mordm.plotpar <- function(data) {
+	last <- data[[length(data)]]
+
+	parcoord(last)
+}
+
+mordm.plotbox <- function(data) {
+	last <- data[[length(data)]]
+	nvars <- attr(data, "nvars")
+	nobjs <- attr(data, "nobjs")
+
+	boxplot(last[,1:nvars])
+}
+
 mordm.plothist <- function(data) {
 	last <- data[[length(data)]]
 	names <- colnames(last)
@@ -99,8 +117,12 @@ mordm.plothist <- function(data) {
 	#}
 }
 
-mordm.plotset <- function(data) {
-	last <- data[[length(data)]]
+mordm.plotset <- function(data, index=-1) {
+	if (index < 1 | index > length(data)) {
+		index = length(data)
+	}
+
+	last <- data[[index]]
 	nvars <- attr(data, "nvars")
 	nobjs <- attr(data, "nobjs")
 	
@@ -108,18 +130,50 @@ mordm.plotset <- function(data) {
 		plot(last[,(nvars+1):(nvars+2)])
 	} else if (nobjs == 3) {
 		plot3d(last[,(nvars+1):(nvars+3)], type="s")
+	} else if (nobjs == 4) {
+		colors <- last[,(nvars+4)]
+		colors <- (colors - min(colors)) / (max(colors) - min(colors))
+
+		plot3d(last[,(nvars+1):(nvars+3)],
+			type="s",
+			col=heat.colors(100)[colors*100],
+			size=1)
 	} else if (nobjs == 5) {
-		scatter3D(x=last[,nvars+1],
-			y=last[,nvars+2],
-			z=last[,nvars+3],
-			colvar=last[,nvars+4],
-			pch=16, cex=1.5,
-			xlab=colnames(last)[nvars+1],
-			ylab=colnames(last)[nvars+2],
-			zlab=colnames(last)[nvars+3],
-			clab=colnames(last)[nvars+4],
-			ticktype="detailed",
-			theta=10, d=2)
+		colors <- last[,(nvars+5)]
+		colors <- (colors - min(colors)) / (max(colors) - min(colors))
+
+		sizes <- last[,(nvars+4)]
+		sizes <- (sizes - min(sizes)) / (max(sizes) - min(sizes))
+
+		plot3d(last[,(nvars+1):(nvars+3)],
+			type="s",
+			col=heat.colors(100)[colors*100],
+			size=sizes*2)
+
+		#identify3d(last[,(nvars+1):(nvars+3)])
+
+		#rgl.setMouseCallbacks(3, begin=function(x, y) { 
+		#	userMatrix <- par3d("userMatrix")
+		#	viewport <- par3d("viewport")
+		#	scale <- par3d("scale")
+		#	projection <- rgl.projection()
+
+		#	d = matrix(nrow=nrow(last), ncol=1)
+
+		#	for (i in 1:nrow(last)) {
+		#		pos <- rgl.user2window(last[i,nvars+1], last[i,nvars+2], last[i,nvars+3], projection=projection)
+		#		d[i] = as.double(dist(rbind(pos[1:2], c(x/viewport[3],1-y/viewport[4]))))
+		#	}
+
+		#	i <- order(d)
+		#	cat("You selected point ")
+		#	cat(i[1])
+		#	cat(" with a distance ")
+		#	cat(d[i[1]])
+		#	cat("\n")
+		#})
+
+		print(selectpoints3d())
 	}
 }
 
@@ -188,12 +242,98 @@ mordm.plotops <- function(data, time=FALSE, improvements=FALSE, log=FALSE) {
 		legend=names, col=colors, lwd=4, bty="o", cex=1.0, horiz=TRUE, xjust=0.5)
 }
 
-data <- mordm.read("E:/Git/openmordm/largeoutput.txt", 20, 5, 1)
-#hypervolume(data[[length(data)]], bandwidth=1, repsperpoint=10)
-mordm.plotops(data, time=FALSE, improvements=TRUE)
-#mordm.plothist(data)
+mordm.correlation <- function(data, ht=0.75, lt=0.25) {
+	last <- data[[length(data)]]
+	names <- colnames(last)
 
-#last <- data[[length(data)]]
+	# compute and classify the correlations
+	correlation.high <- matrix(nrow=0, ncol=3)
+	correlation.inverse <- matrix(nrow=0, ncol=3)
+	correlation.low <- matrix(nrow=0, ncol=3)
+	correlation.medium <- matrix(nrow=0, ncol=3)
+	
+	for (i in 1:(length(names)-1)) {
+		for (j in (i+1):length(names)) {
+			correlation <- cor(last[,i], last[,j])
+
+			if (correlation >= ht) {
+				correlation.high <- rbind(correlation.high, c(i, j, correlation))
+			} else if (correlation <= -ht) {
+				correlation.inverse <- rbind(correlation.inverse, c(i, j, correlation))
+			} else if (abs(correlation) < lt) {
+				correlation.low <- rbind(correlation.low, c(i, j, correlation))
+			} else {
+				correlation.medium <- rbind(correlation.medium, c(i, j, correlation))
+			}
+		}
+	}
+
+	# display the results
+	cat("Summary:\n")
+	cat("    Highly Correlated:    ")
+	cat(nrow(correlation.high))
+	cat("\n")
+	cat("    Inversely Correlated: ")
+	cat(nrow(correlation.inverse))
+	cat("\n")
+	cat("    Weakly Correlated:    ")
+	cat(nrow(correlation.medium))
+	cat("\n")
+	cat("    Uncorrelated:         ")
+	cat(nrow(correlation.low))
+	cat("\n\n")
+
+	cat("High Correlations:\n")
+	if (length(correlation.high) > 0) {
+		for (i in order(correlation.high[,3])) {
+			cat("    ")
+			cat(sprintf("%-8s", names[correlation.high[i,1]]))
+			cat(" <-> ")
+			cat(sprintf("%-8s", names[correlation.high[i,2]]))
+			cat(" (")
+			cat(round(correlation.high[i,3], 2))
+			cat(")\n")
+		}
+	} else {
+		cat("    None\n")
+	}
+
+	cat("\n")
+
+	cat("Inverse Correlations:\n")
+	if (nrow(correlation.inverse) > 0) {
+		for (i in order(correlation.inverse[,3])) {
+			cat("    ")
+			cat(sprintf("%-8s", names[correlation.inverse[i,1]]))
+			cat(" <-> ")
+			cat(sprintf("%-8s", names[correlation.inverse[i,2]]))
+			cat(" (")
+			cat(round(correlation.inverse[i,3], 2))
+			cat(")\n")
+		}
+	} else {
+		cat("    None\n")
+	}
+}
+
+mordm.brush <- function(data, conditions) {
+	attr(data, "brush") <- conditions
+
+	last <- data[[length(data)]]
+	print(min(last[,1]))
+	print(max(last[,1]))
+	print(apply(last, 1, conditions))
+}
+
+data <- mordm.read("E:/Git/openmordm/lakeoutput.txt", 20, 5, 1)
+
+#mordm.plotops(data, time=FALSE, improvements=TRUE)hypervolume(data[[length(data)]], bandwidth=1, repsperpoint=10)
+#mordm.plothist(data)
+mordm.correlation(data)
+#saveGIF({for (i in 1:length(data)) mordm.plotset(data, i)}, convert="gm convert")
+#mordm.plotset(data)
+#mordm.brush(data, function(x) x[1] < 0.02)
+
 #pairs(last)
 #last.prim <- prim.box(x=last[,1:11], y=last[,12], threshold.type=0, threshold=c(0.75, 0.25))
 #summary(last.prim)

13ca0ebf9efe3f26379c89cc2d3925e54eb5eed4
David Hadka <dmh309@arl.psu.edu>
2015-07-23 12:47:24 -0400

    Update copyright info

--- R/plischke.R
+++ R/plischke.R
@@ -1,3 +1,23 @@
+# Copyright 2014-2015 The Pennsylvania State University
+#
+# OpenMORDM was developed by Dr. David Hadka with guidance from Dr. Klaus
+# Keller and Dr. Patrick Reed.  This work was supported by the National
+# Science Foundation through the Network for Sustainable Climate Risk
+# Management (SCRiM) under NSF cooperative agreement GEO-1240507.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Estimation of Borgonovo's Delta Moment Independent Measure
 #
 # This code is a direct translation of Elmar Plischke's original MATLAB code
@@ -9,27 +29,6 @@
 # Please refer to the following paper for details on this method:
 #     Plischke, Borgonovo, Smith: "Global sensitivity measures from given data",
 #     European Journal of Operational Research 226(3):536-550, 2013.
-#
-#
-# MIT License:
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
 kernel.normal <- function(x) exp(-x^2/2)/sqrt(2*pi)
 kernel.triangle <- function(x) pmax(1-abs(x/sqrt(6)), 0)/sqrt(6)
